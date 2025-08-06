bf6207f86bb77a2c6c684afa3403cc3c31867a9e
Douglas Junior <nassifrroma@gmail.com>
2025-08-05 21:36:16 -0300

    start

--- src/registry/RegistryProvider.ts
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-const registryContext = createContext<Record<string, any>>({})
-
-export const RegistryProvider = registryContext.Provider;
-
-export const useInject = <T>(key: string): T => {
-  return useContext(registryContext)[key] as T;
-}
