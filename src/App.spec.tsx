2858b98ecb6f25028f8254cf0f61b07dab6a78cb
Douglas Junior <nassifrroma@gmail.com>
2025-08-06 21:58:58 -0300

    Implementa versão crua da aplicação

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,12 +1,142 @@
 import { describe, expect, test } from 'vitest';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
 describe('App', () => {
-  test('Foo bar', () => {
+  test('Deve testar o fluxo de progresso no preenchimento do fomulário', () => {
     const result = render(<App />)
 
-    expect(result.getByText('TDD e Clean Architecture no frontend com React')).toBeInTheDocument()
+    // Step 1
+    expect(result.getByTestId('span-progress').textContent).toBe('0%');
+    expect(result.getByTestId('span-step').textContent).toBe('1');
+    fireEvent.click(result.getByDisplayValue('administrator'));
+    expect(result.getByTestId('span-progress').textContent).toBe('30%');
+    fireEvent.click(result.getByTestId('button-next'));
+
+    // Step 2
+    expect(result.getByTestId('span-step').textContent).toBe('2');
+    fireEvent.input(result.getByPlaceholderText('Informe seu nome'), { target: { value: 'Douglas Nassif' } });
+    expect(result.getByTestId('span-progress').textContent).toBe('45%');
+    fireEvent.input(result.getByPlaceholderText('Informe seu CPF'), { target: { value: '00011122233' } });
+    expect(result.getByTestId('span-progress').textContent).toBe('60%');
+    fireEvent.input(result.getByPlaceholderText('Informe seu cargo'), { target: { value: 'Programador' } });
+    expect(result.getByTestId('span-progress').textContent).toBe('75%');
+    fireEvent.click(result.getByTestId('button-next'));
+
+    // Step 3
+    expect(result.getByTestId('span-step').textContent).toBe('3');
+    fireEvent.input(result.getByPlaceholderText('Informe seu e-mail'), { target: { value: 'douglas@email.com' } });
+    expect(result.getByTestId('span-progress').textContent).toBe('85%');
+    fireEvent.input(result.getByPlaceholderText('Informe sua senha'), { target: { value: 'senha123' } });
+    expect(result.getByTestId('span-progress').textContent).toBe('95%');
+    fireEvent.input(result.getByPlaceholderText('Repita sua senha'), { target: { value: 'senha123' } })
+    expect(result.getByTestId('span-progress').textContent).toBe('100%');
+
+    // Previous
+    fireEvent.click(result.getByTestId('button-previous'));
+    expect(result.getByTestId('span-step').textContent).toBe('2');
+    fireEvent.click(result.getByTestId('button-previous'));
+    expect(result.getByTestId('span-step').textContent).toBe('1');
+  });
+
+  test('Deve testar a visibilidade dos componentes do formulário', () => {
+    const result = render(<App />)
+
+    // Step 1
+    expect(result.queryByDisplayValue('administrator')).toBeInTheDocument();
+    expect(result.queryByDisplayValue('editor')).toBeInTheDocument();
+    expect(result.queryByDisplayValue('operator')).toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Informe seu nome')).not.toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Informe seu CPF')).not.toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Informe seu cargo')).not.toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Informe seu e-mail')).not.toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Informe sua senha')).not.toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Repita sua senha')).not.toBeInTheDocument();
+    expect(result.queryByTestId('button-previous')).not.toBeInTheDocument();
+    expect(result.queryByTestId('button-next')).toBeInTheDocument();
+    expect(result.queryByTestId('button-confirm')).not.toBeInTheDocument();
+    fireEvent.click(result.getByDisplayValue('administrator'));
+    fireEvent.click(result.getByTestId('button-next'));
+
+    // Step 2
+    expect(result.queryByDisplayValue('administrator')).not.toBeInTheDocument();
+    expect(result.queryByDisplayValue('editor')).not.toBeInTheDocument();
+    expect(result.queryByDisplayValue('operator')).not.toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Informe seu nome')).toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Informe seu CPF')).toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Informe seu cargo')).toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Informe seu e-mail')).not.toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Informe sua senha')).not.toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Repita sua senha')).not.toBeInTheDocument();
+    expect(result.queryByTestId('button-previous')).toBeInTheDocument();
+    expect(result.queryByTestId('button-next')).toBeInTheDocument();
+    expect(result.queryByTestId('button-confirm')).not.toBeInTheDocument();
+    fireEvent.input(result.getByPlaceholderText('Informe seu nome'), { target: { value: 'Douglas Nassif' } });
+    fireEvent.input(result.getByPlaceholderText('Informe seu CPF'), { target: { value: '00011122233' } });
+    fireEvent.input(result.getByPlaceholderText('Informe seu cargo'), { target: { value: 'Programador' } });
+    fireEvent.click(result.getByTestId('button-next'));
+
+    // Step 3
+    expect(result.queryByDisplayValue('administrator')).not.toBeInTheDocument();
+    expect(result.queryByDisplayValue('editor')).not.toBeInTheDocument();
+    expect(result.queryByDisplayValue('operator')).not.toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Informe seu nome')).not.toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Informe seu CPF')).not.toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Informe seu cargo')).not.toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Informe seu e-mail')).toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Informe sua senha')).toBeInTheDocument();
+    expect(result.queryByPlaceholderText('Repita sua senha')).toBeInTheDocument();
+    expect(result.queryByTestId('button-previous')).toBeInTheDocument();
+    expect(result.queryByTestId('button-next')).not.toBeInTheDocument();
+    expect(result.queryByTestId('button-confirm')).toBeInTheDocument();
+  });
+
+  test('Deve testar as validações dos campos e o controle do preenchimento do formulário.', async () => {
+    const result = render(<App />)
+
+    // Step 1
+    fireEvent.click(result.getByTestId('button-next'));
+    expect(result.getByTestId('span-step').textContent).toBe('1');
+    expect(result.getByTestId('span-error').textContent).toBe('Selecione o tipo de conta');
+    fireEvent.click(result.getByDisplayValue('administrator'));
+    fireEvent.click(result.getByTestId('button-next'));
+
+    // Step 2
+    expect(result.getByTestId('span-step').textContent).toBe('2');
+    fireEvent.click(result.getByTestId('button-next'));
+    expect(result.getByTestId('span-step').textContent).toBe('2');
+    expect(result.getByTestId('span-error').textContent).toBe('Preencha o seu nome');
+    fireEvent.input(result.getByPlaceholderText('Informe seu nome'), { target: { value: 'Douglas Nassif' } });
+    fireEvent.click(result.getByTestId('button-next'));
+    expect(result.getByTestId('span-step').textContent).toBe('2');
+    expect(result.getByTestId('span-error').textContent).toBe('Preencha o seu CPF');
+    fireEvent.input(result.getByPlaceholderText('Informe seu CPF'), { target: { value: '00011122233' } });
+    fireEvent.click(result.getByTestId('button-next'));
+    expect(result.getByTestId('span-step').textContent).toBe('2');
+    expect(result.getByTestId('span-error').textContent).toBe('Preencha o seu cargo');
+    fireEvent.input(result.getByPlaceholderText('Informe seu cargo'), { target: { value: 'Programador' } });
+    fireEvent.click(result.getByTestId('button-next'));
+
+    // Step 3
+    expect(result.getByTestId('span-step').textContent).toBe('3');
+    fireEvent.click(result.getByTestId('button-confirm'));
+    expect(result.getByTestId('span-error').textContent).toBe('Preencha o seu e-mail');
+    fireEvent.input(result.getByPlaceholderText('Informe seu e-mail'), { target: { value: 'douglas@email.com' } });
+    fireEvent.click(result.getByTestId('button-confirm'));
+    expect(result.getByTestId('span-error').textContent).toBe('Preencha a sua senha');
+    fireEvent.input(result.getByPlaceholderText('Informe sua senha'), { target: { value: 'senha123' } });
+    fireEvent.click(result.getByTestId('button-confirm'));
+    expect(result.getByTestId('span-error').textContent).toBe('Preencha a sua confirmação de senha');
+    fireEvent.input(result.getByPlaceholderText('Repita sua senha'), { target: { value: 'senha1234' } });
+    fireEvent.click(result.getByTestId('button-confirm'));
+    expect(result.getByTestId('span-error').textContent).toBe('As senhas não conferem');
+    fireEvent.input(result.getByPlaceholderText('Repita sua senha'), { target: { value: 'senha123' } });
+    fireEvent.click(result.getByTestId('button-confirm'));
+
+    // Request
+    const successMessage = await result.findByTestId('span-success');
+    expect(successMessage.textContent).toBe('Conta criada com sucesso #11');
+    expect(successMessage).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,256 @@
+import { ChangeEventHandler, useCallback, useState } from "react"
+import axios from 'axios';
 
 function App() {
+  const [form, setForm] = useState({
+    step: 1,
+    accountType: '',
+    name: '',
+    documentNumber: '',
+    role: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    error: '',
+    success: '',
+  });
+
+  console.log('form', form)
+
+  function calculateProgress() {
+    let progress = 0;
+    if (form.accountType) progress += 30;
+    if (form.name) progress += 15;
+    if (form.documentNumber) progress += 15;
+    if (form.role) progress += 15;
+    if (form.email) progress += 10;
+    if (form.password) progress += 10;
+    if (form.confirmPassword) progress += 5;
+    return progress;
+  }
+
+  const updateForm: ChangeEventHandler<HTMLInputElement> = useCallback((event) => {
+    const { name, value } = event.target
+    setForm(previous => {
+      return {
+        ...previous,
+        [name]: value
+      };
+    })
+  }, []);
+
+  const validate = useCallback(() => {
+    if (form.step === 1 && !form.accountType) {
+      setForm(previous => {
+        return {
+          ...previous,
+          error: 'Selecione o tipo de conta',
+        };
+      });
+      return false;
+    }
+
+    if (form.step === 2) {
+      if (!form.name) {
+        setForm(previous => {
+          return {
+            ...previous,
+            error: 'Preencha o seu nome',
+          };
+        });
+        return false;
+      }
+      if (!form.documentNumber) {
+        setForm(previous => {
+          return {
+            ...previous,
+            error: 'Preencha o seu CPF',
+          };
+        });
+        return false;
+      }
+      if (!form.role) {
+        setForm(previous => {
+          return {
+            ...previous,
+            error: 'Preencha o seu cargo',
+          };
+        });
+        return false;
+      }
+    }
+
+    if (form.step === 3) {
+      if (!form.email) {
+        setForm(previous => {
+          return {
+            ...previous,
+            error: 'Preencha o seu e-mail',
+          };
+        });
+        return false;
+      }
+      if (!form.password) {
+        setForm(previous => {
+          return {
+            ...previous,
+            error: 'Preencha a sua senha',
+          };
+        });
+        return false;
+      }
+      if (!form.confirmPassword) {
+        setForm(previous => {
+          return {
+            ...previous,
+            error: 'Preencha a sua confirmação de senha',
+          };
+        });
+        return false;
+      }
+      if (form.password !== form.confirmPassword) {
+        setForm(previous => {
+          return {
+            ...previous,
+            error: 'As senhas não conferem',
+          };
+        });
+        return false;
+      }
+    }
+
+    setForm(previous => {
+      return {
+        ...previous,
+        error: '',
+      };
+    });
+
+    return true;
+  }, [form]);
+
+  const next = useCallback(() => {
+    if (!validate()) return;
+
+    setForm(previous => {
+      return {
+        ...previous,
+        step: previous.step + 1
+      }
+    });
+  }, [validate]);
+
+  const confirm = useCallback(async () => {
+    if (!validate()) return;
+
+    const body = {
+      accountType: form.accountType,
+      name: form.name,
+      role: form.role,
+      documentNumber: form.documentNumber,
+      email: form.email,
+      password: form.password,
+    };
+
+    const response = await axios.post('https://jsonplaceholder.typicode.com/users', body);
+
+    const successMessage = "Conta criada com sucesso #" + response.data.id;
+    setForm(previous => {
+      return {
+        ...previous,
+        success: successMessage,
+      }
+    });
+  }, [validate, form]);
+
+  const previous = useCallback(() => {
+    setForm(previous => {
+      return {
+        ...previous,
+        step: previous.step - 1
+      }
+    });
+  }, []);
+
   return (
     <div>
-      <h1>TDD e Clean Architecture no frontend com React</h1>
+      <div>
+        <div>
+          <span>
+            Progresso:{' '}
+          </span>
+          <span data-testid="span-progress">{calculateProgress()}%</span>
+        </div>
+        <div>
+          <span>Passo:{' '}</span>
+          <span data-testid="span-step">{form.step}</span>
+        </div>
+        {form.error ? (
+          <div>
+            <span>Erro:{' '}</span>
+            <span data-testid="span-error">{form.error}</span>
+          </div>
+        ) : null}
+        {form.success ? (
+          <div>
+            <span>Sucesso:{' '}</span>
+            <span data-testid="span-success">{form.success}</span>
+          </div>
+        ) : null}
+      </div>
+
+      {/* Step 1 */}
+      {form.step === 1 ? (
+        <div>
+          <input type="radio" id="administrator" name="accountType" value="administrator" onChange={updateForm} checked={form.accountType === 'administrator'} />
+          <label htmlFor="administrator">Administrador</label>
+          <br />
+          <input type="radio" id="editor" name="accountType" value="editor" onChange={updateForm} checked={form.accountType === 'editor'} />
+          <label htmlFor="editor">Editor</label>
+          <br />
+          <input type="radio" id="operator" name="accountType" value="operator" onChange={updateForm} checked={form.accountType === 'operator'} />
+          <label htmlFor="operator">Operador</label>
+        </div>
+      ) : null}
+
+      {/* Step 2 */}
+      {form.step === 2 ? (
+        <div>
+          <label htmlFor="name">Nome:</label>
+          <input type="text" id="name" name="name" placeholder="Informe seu nome" onChange={updateForm} value={form.name} />
+          <br />
+          <label htmlFor="documentNumber">CPF:</label>
+          <input type="text" id="documentNumber" name="documentNumber" placeholder="Informe seu CPF" onChange={updateForm} value={form.documentNumber} />
+          <br />
+          <label htmlFor="role">Cargo:</label>
+          <input type="text" id="role" name="role" placeholder="Informe seu cargo" onChange={updateForm} value={form.role} />
+        </div>
+      ) : null}
+
+      {/* Step 3 */}
+      {form.step === 3 ? (
+        <div>
+          <label htmlFor="email">E-mail</label>
+          <input type="text" id="email" name="email" placeholder="Informe seu e-mail" onChange={updateForm} value={form.email} />
+          <br />
+          <label htmlFor="password">Senha:</label>
+          <input type="password" id="password" name="password" placeholder="Informe sua senha" onChange={updateForm} value={form.password} />
+          <br />
+          <label htmlFor="confirmPassword">Confirmação de senha:</label>
+          <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Repita sua senha" onChange={updateForm} value={form.confirmPassword} />
+        </div>
+      ) : null}
+
+      <div>
+        {form.step > 1 ? (
+          <button data-testid="button-previous" onClick={previous}>Voltar</button>
+        ) : null}
+        {form.step < 3 ? (
+          <button data-testid="button-next" onClick={next}>Próximo</button>
+        ) : (
+          <button data-testid="button-confirm" onClick={confirm}>Confirmar</button>
+        )}
+      </div>
     </div>
   )
 }
